6057cf08eb1f95b6dd50817e720dbb251dd92ad5
mahla-mohamadi <mahlamhmd78@gmail.com>
2023-07-12 09:55:02 +0330

    Initial commit

---
+++ page-templates/source-template.php
@@ -0,0 +1,91 @@
+<?php 
+
+/*
+
+
+
+Template Name: Source Template
+
+
+
+*/
+
+get_header();
+
+?>
+
+<div class="mainView">
+    <div class="generalHeader bottomBorderLight">
+        <h1>Lorem ipsum dolor</h1>
+    </div>
+    <div class="generalHeader bottomBorderDark centeredText">
+        <h2>Lorem ipsum dolor</h2>
+    </div>
+    <h1>Lorem ipsum dolor</h1>
+    <h2>Lorem ipsum dolor</h2>
+    <h3>Lorem ipsum dolor</h3>
+    <h4>Lorem ipsum dolor</h4>
+    <h5>Lorem ipsum dolor</h5>
+    <h6>Lorem ipsum dolor</h6>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
+    <p class="lightText smallText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+    <p class="lighterText smallerText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+    <p class="mainBoxBorder lowMargined lowPadding">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
+    <p class="generalBoxBorder lowMargined lowPadding">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
+    <p class="generalBoxBorder lowRadius mediumPadding mediumMargined">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
+    <p class="generalBoxBorder highRadius highPadding highMargined">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
+    <ul class="generalBoxBorder highRadius lowPadding mediumMargined">
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+        <li>Lorem ipsum dolor</li>
+    </ul>
+    <ul class="mainBoxBorder highRadius lowPadding mediumMargined">
+        <h2 class="mediumWeight">Lorem ipsum</h2>
+        <li class="noListStyle">Lorem ipsum dolor</li>
+        <li class="noListStyle">Lorem ipsum dolor</li>
+        <li class="noListStyle">Lorem ipsum dolor</li>
+        <li class="noListStyle">Lorem ipsum dolor</li>
+        <li class="noListStyle">Lorem ipsum dolor</li>
+    </ul>
+    <ul>
+        <li class="checkListStyle">checkListStyle</li>
+        <li class="crossListStyle">crossListStyle</li>
+        <li class="arrowListStyle">crossListStyle</li>
+    </ul>
+    <div class="generalBoxBorder highRadius lowPadding mediumMargined">
+        <form action="">
+            <label>Name<input placeholder="Your Name" type="text" name="" id=""></label>
+            <label>Email<input placeholder="Your Email" type="text" name="" id=""></label>
+            <label>Phone<input placeholder="Your Phone" type="text" name="" id=""></label>
+            <label>Password<input placeholder="Your Pass" type="text" name="" id=""></label>
+            <label>Description<textarea placeholder="Your Massage" name="" id="" cols="30" rows="10"></textarea></label>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+    <table>
+        <tr><th>Lorem ipsum</th><th>Lorem ipsum</th><th>Lorem ipsum</th><th>Lorem ipsum</th><th>Lorem ipsum</th></tr>
+        <tr><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td></tr>
+        <tr><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td></tr>
+        <tr><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td></tr>
+        <tr><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td><td>Lorem ipsum dolor sit</td></tr>
+    </table>
+    <div style="display:flex;">
+        <a class="generalButton" href="#">More...</a>
+        <a class="generalButton outlineButton" href="#">More...</a>
+    </div>
+    <div style="display:flex;">
+        <button class="generalButton">More...</button>
+        <button class="generalButton outlineButton">More...</button>
+    </div>
+    <div class="siteHashtags">Lorem ipsum</div>
+    <div class="siteMessage warningMessage">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed</div>
+    <div class="siteMessage successMessage">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed</div>
+    <div class="siteMessage errorMessage">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed</div>
+    <div class="siteMessage informMessage">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed</div>
+</div>
+
+
+<?php get_footer();
